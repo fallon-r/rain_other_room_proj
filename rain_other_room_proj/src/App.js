fcc2683e479d76056629aadcadb7d726d24c35a3
rf-spuds <rfspuds@gmail.com>
2020-10-18 06:15:55 -0400

    React skeleton/testing ground

---
+++ rain_other_room_proj/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Spotify Web Playback SDK Quick Start Tutorial</h1>
+     <h2>Open your console log</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
